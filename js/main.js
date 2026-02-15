ea101922fa4a8d127c7257ea221d7f1ede53eecb
kkeeeen <80876188+kkeeeen@users.noreply.github.com>
2026-02-15 22:40:38 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY VELENTINES!!').split('')
+    const titles = ('HAPPY <3 DAY!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
